13078ffa91d80ce794f760853797dcff3910b382
aghanianh <aghanyan123@gmail.com>
2025-01-21 20:16:34 +0400

    Change logic

--- main.tf
+++ main.tf
@@ -4,63 +4,64 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_internet_gateway" "gw" {
+    count = (length(var.public_subnets) > 0  || length(var.private_subnets) > 0) ? 1 : 0
     vpc_id = aws_vpc.main.id
 }
 
 resource "aws_eip" "ng_eip" {
+    count = length(var.private_subnets) > 0 ? 1 : 0
     vpc = true
-}
+}   
 
 resource "aws_nat_gateway" "ng" {
-    subnet_id = aws_subnet.public_subnet
-    allocation_id = aws_eip.ng_eip.id 
-    depends_on = [ aws_internet_gateway.gw ]
-
+    count = length(var.private_subnets)
+    subnet_id = aws_subnet.public_subnet[0].id
+    allocation_id = aws_eip.ng_eip[0].id 
 }
 
 
 resource "aws_subnet" "public_subnet" {
+    count = length(var.public_subnets)
     vpc_id = aws_vpc.main.id
-    cidr_block = var.public_subnet
-    availability_zone = local.av_z
+    cidr_block = var.public_subnets[count.index]
+    availability_zone = var.av_z[count.index]
     map_public_ip_on_launch = true 
-    
+
 }
 
+
 resource "aws_subnet" "private_subnet" {
+    count = length(var.private_subnets)
     vpc_id = aws_vpc.main.id 
-    cidr_block = var.private_subnet
-    availability_zone = local.av_z
+    cidr_block = var.private_subnets[count.index]
+    availability_zone = var.av_z[count.index]
     map_public_ip_on_launch = false 
 }
 
 resource "aws_route_table" "public_route" {
+    count = length(var.public_subnets) > 0 ? 1 : 0
     vpc_id = aws_vpc.main.id
     route  {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.gw.id
+        gateway_id = aws_internet_gateway.gw[0].id
     }
 }
 
-
-
 resource "aws_route_table" "private_route" {
+    count = length(var.private_subnets) > 0 ? 1 : 0
     vpc_id = aws_vpc.main.id 
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.ng.id 
+        gateway_id = aws_nat_gateway.ng[0].id 
     }
 }
 
 resource "aws_route_table_association" "public_subnet" {
-    subnet_id = aws_subnet.public_subnet.id 
-    route_table_id = aws_route_table.public_route.id 
+    subnet_id = aws_subnet.public_subnet[0].id 
+    route_table_id = aws_route_table.public_route[0].id 
 }
 
 resource "aws_route_table_association" "private_subnet" {
-    subnet_id = aws_subnet.private_subnet.id 
-    route_table_id = aws_route_table.private_route.id
+    subnet_id = aws_subnet.private_subnet[0].id 
+    route_table_id = aws_route_table.private_route[0].id
 }
-
-
-
--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-data "aws_region" "current_reg" {}
 data "aws_availability_zones" "available" {
     state = "available"
 }
@@ -8,20 +7,26 @@ variable "cidr_block" {
     default = "10.0.0.0/16"
 }
 
-variable "public_subnet" {
-    type = string 
-    default = "10.0.1.0/24"
+variable "public_subnets" {
+    type = list(string)
+    default = ["10.0.1.0/24"]
 }
 
-variable "private_subnet" {
+variable "private_subnets" {
+    type = list(string)
+    default = ["10.0.2.0/24"]
+}
+variable "aws_region" {
     type = string 
-    default = "10.0.2.0/24"
+    default = "us-east"
 }
 
-locals  {
-    av_z = data.aws_availability_zones.available.names[0]
-    aws_region = data.aws_region.current_reg.name
+variable "av_z" {
+    type = list(string)
+    default = ["us-east-1a"]
 }
+
+
 #aws regiony user input , ete availability zone- sxale exacic patahakan dne , nat gateway... sax user input 
 #subnetneri qanaky ( = availabil zone-eri vra ), petq e subnet te che (ete priv subnet chka nat gw petq che)
-#
+#foreach count ....
